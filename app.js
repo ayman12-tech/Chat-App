a81f8e548fcd1dc47d883d05f523190640ba4cd4
ayman12-tech <syedaayman8@gmail.com>
2020-09-29 01:36:25 +0500

    Commit Complete

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 var myName = prompt("Enter your name");
+function sendMessage(){
+    //get message
+    var message=document.getElementById("message").value;
+    
+    //save in database
+    firebase.database().ref("message").push().set({
+        "sender":myName,
+        "message":message
+    });
+
+    //prevent form from submitting
+    return false;
+}
+
+//listen to upcoming messages:
+firebase.database().ref("message").on("child_added",function(snapshot){
+var html="";
+html+="<li>";
+html+=snapshot.val().sender + ": " + snapshot.val().message;
+html+="</li>";
+document.getElementById("messages").innerHTML+=html;
+});
