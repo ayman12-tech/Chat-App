9074cb74baca7e2c9bff82baea43893781f46b43
ayman12-tech <syedaayman8@gmail.com>
2020-09-29 17:36:35 +0500

    Commit Complete

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function sendMessage(){
     var message=document.getElementById("message").value;
     
     //save in database
-    firebase.database().ref("message").push().set({
+    firebase.database().ref("messages").push().set({
         "sender":myName,
         "message":message
     });
@@ -13,11 +13,32 @@ function sendMessage(){
     return false;
 }
 
-//listen to upcoming messages:
-firebase.database().ref("message").on("child_added",function(snapshot){
+//listen for incoming messages:
+firebase.database().ref("messages").on("child_added",function(snapshot){
 var html="";
-html+="<li>";
+//give each message a unique id
+
+html+="<li id='message-" +snapshot.key+"'>";
+//show delete button if message is sent.
+if (snapshot.val().sender==myName){
+    html+="<button data-id='"+snapshot.key +"' onclick='deleteMessage(this);'>";
+        html+="Delete";
+    html+="</button>";
+}
 html+=snapshot.val().sender + ": " + snapshot.val().message;
 html+="</li>";
 document.getElementById("messages").innerHTML+=html;
+});
+
+function deleteMessage(self){
+    //get message ID
+    var messageID= self.getAttribute("data-id");
+    //delete message
+    firebase.database().ref("messages").child(messageID).remove();
+}
+
+//attach listener for delete message
+firebase.database().ref("messages").on("child_removed",function (snapshot){
+    //remove message node
+    document.getElementById("message-" + snapshot.key).innerHTML="This message has been removed.";
 });
